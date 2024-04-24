b6aac8a32d043d58227e8bdf2e8a40ba5ec382d1
marcos <mmunoz2@cendrassos.net>
2024-04-24 16:15:14 +0200

    Delete Friends #61

--- app/Http/Controllers/FriendsController.php
+++ app/Http/Controllers/FriendsController.php
@@ -64,6 +64,7 @@ class FriendsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
+    
 public function destroy(string $id)
 {
     $user = auth()->user();
